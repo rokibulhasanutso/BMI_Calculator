dda6d4e48b7b42178a2d60519867ba9ee29e49e6
rokibulhasan <rokibulhasanutsoo@gmail.com>
2023-03-29 02:14:21 +0600

    bmi calculator

---
+++ script.js
@@ -0,0 +1,10 @@
+const calculateBtn = document.getElementById("calculateBtn");
+const resultElement = document.getElementById("result");
+
+calculateBtn.addEventListener("click", function() {
+  const weight = document.getElementById("weightInput").value;
+  const height = document.getElementById("heightInput").value / 100;
+  let bmi = (weight / (height * height)).toFixed(2);
+  (bmi == "NaN")? bmi='0': bmi;
+  resultElement.innerHTML = `Your BMI is <p>${bmi}</p>`;
+});
