998cbcfa8596cb7bc0a114ffcb305691ce1761ab
Rokibul Hasan Utso <rokibulhasanutsoo@gmail.com>
2023-03-29 20:06:16 +0600

    remove height 100 height division

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const resultElement = document.getElementById("result");
 
 calculateBtn.addEventListener("click", function() {
   const weight = document.getElementById("weightInput").value;
-  const height = document.getElementById("heightInput").value / 100;
+  const height = document.getElementById("heightInput").value;
   let bmi = (weight / (height * height)).toFixed(2);
   (bmi == "NaN")? bmi='0': bmi;
   resultElement.innerHTML = `Your BMI is <p>${bmi}</p>`;
